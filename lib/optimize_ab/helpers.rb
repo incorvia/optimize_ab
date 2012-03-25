4e7139a063d5b56eaa0f8a0832e4130c82310e0f
Kevin Incorvia <incorvia@gmail.com>
2012-03-24 22:22:02 -0400

    update names

--- lib/optimize_ab/helpers.rb
+++ lib/optimize_ab/helpers.rb
@@ -41,23 +41,16 @@ module Helpers
   #   <%= multi_end_section %>
   # </h1>
   #
+  # in HAML a link might be written as:
   #
-  # Conversion Script
+  # = link_to (multi_begin_section('Headline') + "Welcome" + multi_end_section)
+  #
+  #
+  # OnClick Conversion  
   # ---------------------------------------
-  # The conversion script tracks visits to your conversion page and
-  # should be placed immediately before the closing </head> tag on
-  # your conversion page.
-  #
-  #
-  # OnClick Conversion Script
-  # ---------------------------------------
-  # The conversion script tracks visits to your conversion page and
-  # should be placed immediately before the closing </head> tag on
-  # your conversion page.
-  #
-  # Next, you'll need to add a snippet of text to the link or links
-  #  that you want to track as a conversion. Let's say your link
-  #  looks like this:
+  # You'll need to add a snippet of text to the link or links
+  # that you want to track as a conversion. Let's say your link
+  # looks like this:
   # 
   # <a href="http://www.example.com/promotion">Featured Products</a>
   #
@@ -78,85 +71,25 @@ module Helpers
   ####################################################################
 
 
-  ######## Multi-Variate Control & Tracking Script ########
+  ######## Control / Tracking Script / Click Function ########
 
-  def multi_head(k, ua, pv)
+  def ab_head(k, ua, pv)
     js = <<-HTML
-      <!-- Google Website Optimizer Control Script -->
       <script>
-      function utmx_section(){}function utmx(){}
-      (function(){var k="#{k}",d=document,l=d.location,c=d.cookie;function f(n){
-      if(c){var i=c.indexOf(n+'=');if(i>-1){var j=c.indexOf(';',i);return escape(c.substring(i+n.
-      length+1,j<0?c.length:j))}}}var x=f('__utmx'),xx=f('__utmxx'),h=l.hash;
-      d.write('<sc'+'ript src="'+
-      'http'+(l.protocol=='https:'?'s://ssl':'://www')+'.google-analytics.com'
-      +'/siteopt.js?v=1&utmxkey='+k+'&utmx='+(x?x:'')+'&utmxx='+(xx?xx:'')+'&utmxtime='
-      +new Date().valueOf()+(h?'&utmxhash='+escape(h.substr(1)):'')+
-      '" type="text/javascript" charset="utf-8"></sc'+'ript>')})();
-      </script><script>utmx("url",'A/B');</script>
-      <!-- End of Google Website Optimizer Control Script -->
-      <!-- Google Website Optimizer Tracking Script -->
+        function utmx_section(){}function utmx(){}
+        (function(){var k="#{k}",d=document,l=d.location,c=d.cookie;function f(n){
+        if(c){var i=c.indexOf(n+'=');if(i>-1){var j=c.indexOf(';',i);return escape(c.substring(i+n.
+        length+1,j<0?c.length:j))}}}var x=f('__utmx'),xx=f('__utmxx'),h=l.hash;
+        d.write('<sc'+'ript src="'+
+        'http'+(l.protocol=='https:'?'s://ssl':'://www')+'.google-analytics.com'
+        +'/siteopt.js?v=1&utmxkey='+k+'&utmx='+(x?x:'')+'&utmxx='+(xx?xx:'')+'&utmxtime='
+        +new Date().valueOf()+(h?'&utmxhash='+escape(h.substr(1)):'')+
+        '" type="text/javascript" charset="utf-8"></sc'+'ript>')})();
+      </script>
       <script type="text/javascript">
         var _gaq = _gaq || [];
         _gaq.push(['gwo._setAccount', "#{ua}"]);
         _gaq.push(['gwo._trackPageview', "#{pv}"]);
-        (function() {
-          var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-          ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-          var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-        })();
-      </script>
-      <!-- End of Google Website Optimizer Tracking Script -->
-    HTML
-    return js.html_safe
-  end
-
-
-  ######## Page Sections Scripts ########
-
-  def multi_begin_section(name)
-    js = <<-HTML
-      <script>utmx_section("#{name}")</script>
-    HTML
-    return js.html_safe
-  end
-
-  def multi_end_section
-    js = <<-HTML
-      </noscript>
-    HTML
-    return js.html_safe
-  end
-
-
-  ######## Mutli-Conversion Script ########
-
-  def multi_converstion(ua, pv)
-    js = <<-HTML
-      <!-- Google Website Optimizer Tracking Script -->
-      <script type="text/javascript">
-        var _gaq = _gaq || [];
-        _gaq.push(['gwo._setAccount', "#{ua}"]);
-        _gaq.push(['gwo._trackPageview', "#{pv}"]);
-        (function() {
-          var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-          ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-          var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-        })();
-      </script>
-      <!-- End of Google Website Optimizer Tracking Script -->
-    HTML
-    return js.html_safe
-  end
-
-  ######## OnClick Conversion Script ########
-
-  def multi_on_click_converstion(ua, pv)
-    js = <<-HTML
-      <!-- Google Website Optimizer Tracking Script -->
-       <script type="text/javascript">
-        var _gaq = _gaq || [];
-        _gaq.push(['gwo._setAccount', "#{ua}"]);
         function doGoal(that) {
          try {
           _gaq.push(['gwo._trackPageview', "#{pv}"]);
@@ -165,12 +98,28 @@ module Helpers
          catch(err){}
         }
         (function() {
-         var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-         ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-         var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+          var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+          ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+          var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
         })();
-       </script>
-      <!-- End of Google Website Optimizer Tracking Script -->
+      </script>
+    HTML
+    return js.html_safe
+  end
+
+
+  ######## Page Sections Scripts ########
+
+  def ab_section(name)
+    js = <<-HTML
+      <script>utmx_section("#{name}")</script>
+    HTML
+    return js.html_safe
+  end
+
+  def ab_section
+    js = <<-HTML
+      </noscript>
     HTML
     return js.html_safe
   end
